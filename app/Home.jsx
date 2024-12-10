dc9a49ea1bbf0bec31becf9ccdee6ab0934c7ee9
malik <malikyakub@hotmail.com>
2024-12-10 19:30:39 +0300

    hero cards completed

--- app/Home.jsx
+++ app/Home.jsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Linking } from "react-native";
 import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import colors from "../assets/colors/colors";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
 import BottomNav from "../components/BottomNav";
-import QuickLinksCard from "../components/QuickLinksCard";
+import HeroCards from "../components/HeroCards";
 
 const home = () => {
   return (
@@ -13,7 +14,13 @@ const home = () => {
       <View style={styles.hero}>
         <Text style={styles.text}>With </Text>
         <Text style={styles.logo}>Mk-passes</Text>
-        <QuickLinksCard img={require("../assets/images/passwords-ill.png")} />
+        <HeroCards />
+        <AntDesign
+          onPress={() => Linking.openURL("https://mk-yakub.netlify.app")}
+          style={styles.heart}
+          name="heart"
+          color={colors["darkest-pri"]}
+        />
       </View>
       <View>
         <BottomNav current={"home"} />
@@ -28,6 +35,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.primary,
+    justifyContent: "center",
   },
   hero: {
     flex: 1,
@@ -36,13 +44,22 @@ const styles = StyleSheet.create({
     zIndex: 1,
   },
   text: {
-    fontSize: 40,
+    fontSize: 30,
     fontFamily: "Jaldi",
     color: colors.dark,
   },
   logo: {
-    fontSize: 80,
+    fontSize: 60,
     fontFamily: "Jaini",
     color: colors.dark,
+    lineHeight: 60,
+  },
+  heroCards: {
+    marginBlock: 20,
+  },
+  heart: {
+    fontSize: 20,
+    padding: 10,
+    textAlign: "center",
   },
 });
